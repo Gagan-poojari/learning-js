ac92c32a7e85ef82767f6a8bb108316d86ea4169
Gagan-poojari <159212673+Gagan-poojari@users.noreply.github.com>
2024-02-28 11:27:53 +0530

    Add files via upload

---
+++ exercise14.js
@@ -0,0 +1,81 @@
+let rno1 = Math.trunc(Math.random() * 5000)
+let rno2 = rno1 + Math.trunc(Math.random() * 5000)
+let rno3 = rno2 + Math.trunc(Math.random() * 5000)
+let rno4 = rno3 + Math.trunc(Math.random() * 5000)
+let rno5 = rno4 + Math.trunc(Math.random() * 5000)
+
+
+setTimeout(() => {
+    document.querySelector(".text1").innerHTML = "Initializing hacking"
+}, rno1);
+
+setTimeout(() => {
+    document.querySelector(".dot11").innerHTML = "."
+}, rno1 +500);
+setTimeout(() => {
+    document.querySelector(".dot12").innerHTML = "."
+}, rno1 +1000);
+setTimeout(() => {
+    document.querySelector(".dot13").innerHTML = "."
+}, rno1 +1500);
+
+
+setTimeout(() => {
+    document.querySelector(".text2").innerHTML = "Reading your files"
+}, rno2);
+
+setTimeout(() => {
+    document.querySelector(".dot21").innerHTML = "."
+}, rno2 +500);
+setTimeout(() => {
+    document.querySelector(".dot22").innerHTML = "."
+}, rno2 +1000);
+setTimeout(() => {
+    document.querySelector(".dot23").innerHTML = "."
+}, rno2 +1500);
+
+
+setTimeout(() => {
+    document.querySelector(".text3").innerHTML = "Password files detected"
+}, rno3);
+
+setTimeout(() => {
+    document.querySelector(".dot31").innerHTML = "."
+}, rno3 +500);
+setTimeout(() => {
+    document.querySelector(".dot32").innerHTML = "."
+}, rno3 +1000);
+setTimeout(() => {
+    document.querySelector(".dot33").innerHTML = "."
+}, rno3 +1500);
+
+
+setTimeout(() => {
+    document.querySelector(".text4").innerHTML = "Sending all files to server"
+}, rno4);
+
+setTimeout(() => {
+    document.querySelector(".dot41").innerHTML = "."
+}, rno4 +500);
+setTimeout(() => {
+    document.querySelector(".dot42").innerHTML = "."
+}, rno4 +1000);
+setTimeout(() => {
+    document.querySelector(".dot43").innerHTML = "."
+}, rno4 +1500);
+
+
+setTimeout(() => {
+    document.querySelector(".text5").innerHTML = "Cleaning up"
+}, rno5);
+
+setTimeout(() => {
+    document.querySelector(".dot51").innerHTML = "."
+}, rno5 +500);
+setTimeout(() => {
+    document.querySelector(".dot52").innerHTML = "."
+}, rno5 +1000);
+setTimeout(() => {
+    document.querySelector(".dot53").innerHTML = "."
+}, rno5 +1500);
+
